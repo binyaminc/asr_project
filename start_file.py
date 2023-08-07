ef2a025f8ba24f4989783c267c66d874474af5b8
bin <binyaminc6@gmail.com>
2023-08-07 21:18:46 +0300

    change nn to fit the data using cnn
    We are heading to Sequence-Level Processing. Meanwhile using CNN, maybe later using transformers.

--- start_file.py
+++ start_file.py
@@ -23,7 +23,7 @@ train_path = r'an4\\train\\an4\\'
 class CharacterDetectionNet(nn.Module):
     def __init__(self, classifierArgs):
         super().__init__()
-        self.flatten = nn.Flatten()
+        self.flatten = nn.Flatten(start_dim=1, end_dim=2)
         conv_kernels = classifierArgs.kernels_per_layer
         self.conv0 = nn.Conv2d(1, conv_kernels[0], 3, padding=1)
         self.conv1 = nn.Conv2d(conv_kernels[1 - 1], conv_kernels[1], 3, padding=1)
@@ -33,8 +33,9 @@ class CharacterDetectionNet(nn.Module):
         self.conv5 = nn.Conv2d(conv_kernels[5 - 1], conv_kernels[5], 3, padding=1)
         self.conv6 = nn.Conv2d(conv_kernels[6 - 1], conv_kernels[6], 3, padding=1)
         self.relu = nn.ReLU()
-        self.maxpooling = nn.MaxPool2d(2)  # TODO: 3?
-        self.linear = nn.Linear(4*8 * conv_kernels[-1], len(alphabet))
+        self.softmax = nn.Softmax(dim=1)
+        self.maxpooling = nn.MaxPool2d(kernel_size=(2, 1))
+        self.linear = nn.Linear(1024, len(alphabet))
 
         # a more pythonic way to go about this:
         # self.convs = [nn.Conv1d(1, conv_kernels[0], 3, padding=1)] + \
@@ -57,11 +58,11 @@ class CharacterDetectionNet(nn.Module):
         print(x.shape)
         x = self.maxpooling(self.relu(self.conv6(x)))
         print(x.shape)
-        x = self.flatten(x)
+        x = self.flatten(x).permute(2,0,1)
         print(x.shape)
         x = self.linear(x)
-        
-        x = nn.Softmax(x, dim=1)
+        print(x.shape)
+        x = self.softmax(x)
         print(x.shape)
 
         return x
