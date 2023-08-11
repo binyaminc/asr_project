cf5d5412b1bccadabe5beb7b4650474b2f26d2c6
bin <binyaminc6@gmail.com>
2023-08-11 13:48:21 +0300

    get back 2 cnn layers

--- networks.py
+++ networks.py
@@ -21,12 +21,12 @@ class CharNet_1(nn.Module):
         self.conv5 = nn.Conv2d(conv_kernels[5 - 1], conv_kernels[5], 3, padding=1)
         # self.conv6 = nn.Conv2d(conv_kernels[6 - 1], conv_kernels[6], 3, padding=1)
         
-        self.conv6 = nn.Conv2d(conv_kernels[2 - 1], conv_kernels[6], 3, padding=1)
+        self.conv6 = nn.Conv2d(conv_kernels[4 - 1], conv_kernels[6], 3, padding=1)
         
         self.relu = nn.ReLU()
         self.maxpooling = nn.MaxPool2d(kernel_size=(2, 1))
         # self.linear = nn.Linear(1024, len(index2char))
-        self.linear = nn.Linear(16384, len(index2char))
+        self.linear = nn.Linear(4096, len(index2char))
 
         # a more pythonic way to go about this:
         # self.convs = [nn.Conv1d(1, conv_kernels[0], 3, padding=1)] + \
@@ -37,8 +37,8 @@ class CharNet_1(nn.Module):
         x = x.permute(0, 1, 3, 2)
         x = self.relu(self.conv0(x))
         x = self.relu(self.conv1(x))
-        # x = self.maxpooling(self.relu(self.conv2(x)))
-        # x = self.maxpooling(self.relu(self.conv3(x)))
+        x = self.maxpooling(self.relu(self.conv2(x)))
+        x = self.maxpooling(self.relu(self.conv3(x)))
         # x = self.maxpooling(self.relu(self.conv4(x)))
         # x = self.maxpooling(self.relu(self.conv5(x)))
         x = self.maxpooling(self.relu(self.conv6(x)))
