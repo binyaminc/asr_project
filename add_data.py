d7128a084f024ca24dfc9213322c21b302e53563
bin <binyaminc6@gmail.com>
2023-08-13 16:47:16 +0300

    create new data

---
+++ add_data.py
@@ -0,0 +1,75 @@
+import os
+import wave
+import random
+
+wav_path = r'an4\\train\\an4\\wav\\'
+txt_path = r'an4\\train\\an4\\txt\\'
+longest_num_frames = 102400
+
+def main():
+    # List all files in the folder
+    wav_files = [file for file in os.listdir(wav_path) if file.endswith('.wav')]
+    
+    for _ in range(4000):
+        file1_path, file2_path = random.sample(wav_files, 2)
+        
+        # Open the first WAV file
+        wav_file1 = wave.open(wav_path + file1_path, 'rb')
+        num_frames1 = wav_file1.getnframes()
+        params1 = wav_file1.getparams()
+
+        # Open the second WAV file
+        wav_file2 = wave.open(wav_path + file2_path, 'rb')
+        num_frames2 = wav_file2.getnframes()
+        params2 = wav_file2.getparams()
+
+        # Check if the combined num_frames is smaller than the maximum size
+        if num_frames1 + num_frames2 < longest_num_frames:
+            # Create a new combined WAV file
+            output_path1 = file1_path[:-4] + '_' + file2_path
+            with wave.open(wav_path + output_path1, 'wb') as combined_wav:
+                combined_wav.setparams(params1)  # Use parameters from the first file
+                combined_wav.writeframes(wav_file1.readframes(num_frames1))
+                combined_wav.writeframes(wav_file2.readframes(num_frames2))
+
+            # Create a new combined WAV file
+            output_path2 = file2_path[:-4] + '_' + file1_path
+            with wave.open(wav_path + output_path2, 'wb') as combined_wav:
+                combined_wav.setparams(params1)  # Use parameters from the first file
+                combined_wav.writeframes(wav_file2.readframes(num_frames2))
+                combined_wav.writeframes(wav_file1.readframes(num_frames1))
+
+            # create a new combined txt file
+            file1_path, file2_path = file1_path[:-4] + '.txt', file2_path[:-4] + '.txt'
+            with open(txt_path + file1_path, 'r') as file1, open(txt_path + file2_path, 'r') as file2:
+                data1 = file1.read()
+                data2 = file2.read()
+
+            with open(txt_path + file1_path[:-4] + '_' + file2_path, 'w') as destination_file:
+                destination_file.write(data1 + ' ' + data2)
+            
+            with open(txt_path + file2_path[:-4] + '_' + file1_path, 'w') as destination_file:
+                destination_file.write(data2 + ' ' + data1)
+                
+
+def find_longest_file():
+    # List all files in the folder
+    wav_files = [file for file in os.listdir(wav_path) if file.endswith('.wav')]
+    
+    biggest = 0
+    file = ''
+    
+    # Loop through each WAV file and get the number of frames
+    for wav_file in wav_files:
+        wav_file_path = os.path.join(wav_path, wav_file)
+        with wave.open(wav_file_path, 'rb') as wav_file_obj:
+            num_frames = wav_file_obj.getnframes()
+            if num_frames > biggest:
+                biggest = num_frames
+                file = wav_file
+            print(f"WAV File: {wav_file}, Frames: {num_frames}")
+    print(f"biggest: {file}, frames: {biggest}")
+
+
+if __name__ == "__main__":
+    main()
--- networks.py
+++ networks.py
@@ -19,14 +19,14 @@ class CharNet_1(nn.Module):
         self.conv3 = nn.Conv2d(conv_kernels[3 - 1], conv_kernels[3], 3, padding=1)
         self.conv4 = nn.Conv2d(conv_kernels[4 - 1], conv_kernels[4], 3, padding=1)
         self.conv5 = nn.Conv2d(conv_kernels[5 - 1], conv_kernels[5], 3, padding=1)
-        # self.conv6 = nn.Conv2d(conv_kernels[6 - 1], conv_kernels[6], 3, padding=1)
+        self.conv6 = nn.Conv2d(conv_kernels[6 - 1], conv_kernels[6], 3, padding=1)
         
-        self.conv6 = nn.Conv2d(conv_kernels[4 - 1], conv_kernels[6], 3, padding=1)
+        # self.conv6 = nn.Conv2d(conv_kernels[4 - 1], conv_kernels[6], 3, padding=1)
         
         self.relu = nn.ReLU()
         self.maxpooling = nn.MaxPool2d(kernel_size=(2, 1))
-        # self.linear = nn.Linear(1024, len(index2char))
-        self.linear = nn.Linear(4096, len(index2char))
+        self.linear = nn.Linear(1024, len(index2char))
+        # self.linear = nn.Linear(4096, len(index2char))
 
         # a more pythonic way to go about this:
         # self.convs = [nn.Conv1d(1, conv_kernels[0], 3, padding=1)] + \
@@ -39,8 +39,8 @@ class CharNet_1(nn.Module):
         x = self.relu(self.conv1(x))
         x = self.maxpooling(self.relu(self.conv2(x)))
         x = self.maxpooling(self.relu(self.conv3(x)))
-        # x = self.maxpooling(self.relu(self.conv4(x)))
-        # x = self.maxpooling(self.relu(self.conv5(x)))
+        x = self.maxpooling(self.relu(self.conv4(x)))
+        x = self.maxpooling(self.relu(self.conv5(x)))
         x = self.maxpooling(self.relu(self.conv6(x)))
         x = self.flatten(x)
         x = x.permute(2, 0, 1)
